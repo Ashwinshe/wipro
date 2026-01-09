131097d4c2a52c57feec46647a830f780cb58291
Ashwinshe <ashwin.shedge.197006@gmail.com>
2026-01-09 18:37:55 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-// Console.WriteLine("Please enter your name.");
-// String name = Console.ReadLine();
-// Console.WriteLine("Hello, " + name);
-Console.WriteLine("Please enter your age:");
-string ageIn = Console.ReadLine();
+using System;
+using System.Collections.Generic;
 
-int age;
-bool isValid = int.TryParse(ageIn, out age);
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Order> orderList = new List<Order>();
 
-if (isValid)
-{
-    if (age >= 18)
-    {
-        Console.WriteLine("Eligible");
+            orderList.Add(new Order
+            {
+                OrderId = 1,
+                ProductName = "Laptop",
+                Quantity = 2,
+                Price = 1500
+            });
+
+            orderList.Add(new Order
+            {
+                OrderId = 2,
+                ProductName = "Mobile",
+                Quantity = 5,
+                Price = 800
+            });
+
+            Console.WriteLine("Orders in the Order Collection:");
+            foreach (var order in orderList)
+            {
+                Console.WriteLine(order);
+            }
+
+            Console.ReadLine(); 
+        }
     }
-    else
-    {
-        Console.WriteLine("Not eligible");
-    }
-}
-else
-{
-    Console.WriteLine("Invalid input");
 }
